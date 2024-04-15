2c21c67c5052123a5bb0db9c7624b97d4d6117da
tkpy543 <takspooh@gmail.com>
2024-04-16 01:34:21 +0900

    change index.js for crypto experiment

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const {SignJWT} = require('jose')
+const {createPrivateKey} = require('node:crypto')
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+/**
+ * 180 days
+ */
+const expires_in = 86400 * 180
+const exp = Math.ceil(Date.now() / 1000) + expires_in
+
+(async function(){
+  try {
+    // args for creating secret
+    const kid = core.getInput('kid')
+    const iss = core.getInput('iss')
+    const sub = core.getInput('sub')
+    const private_key = core.getInput('private_key')
+    console.log('args: ', {kid, iss, sub, private_key})
+    if (!kid || !iss || !sub || !private_key) {
+      throw new Error('parameter required!!, ', {
+        kid: !kid,
+        iss: !iss,
+        sub: !sub,
+        private_key: !private_key
+      })
+    }
+    console.log(`Valid until: ${new Date(exp * 1000)}`)
+    const time = await (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+  
+})()
